4cf3bff41c22d9fbef84aff4df446d38615fec3f
JOnMaybe <jonathan.hodgen@gmail.com>
2024-08-22 16:01:07 -0600

    first

---
+++ pipe.lua
@@ -0,0 +1,32 @@
+local modem = peripheral.find("modem")
+ 
+local myNote = "F#3"
+ 
+modem.open(65535)
+ 
+function mysplit(inputstr, sep)
+    if sep == nil then
+      sep = "%s"
+    end
+    local t = {}
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+      table.insert(t, str)
+    end
+    return t
+end
+
+while true do
+   local id, msg = rednet.receive(nil, 5)
+   if msg ~= nil and msg ~= "" then 
+        local notesInMsg = mysplit(msg, ",")
+            local playing = false
+        for _,v in pairs(notesInMsg) do
+            if v == myNote then
+            playing = true
+            break
+            end
+        end
+        redstone.setOutput("top", playing);
+        
+    end
+end
